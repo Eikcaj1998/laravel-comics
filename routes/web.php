5c4a7d0b0ea4eac9974b0618031f89bd3de4f67a
Eikcaj.1998 <jw220998@gmail.com>
2022-09-15 16:18:04 +0200

    add img and file php

--- routes/web.php
+++ routes/web.php
@@ -14,5 +14,18 @@ use Illuminate\Support\Facades\Route;
 */
 
 Route::get('/', function () {
-    return view('welcome');
+    $title = 'home';
+    return view('comics',compact('title'));
+});
+Route::get('/', function () {
+    return view('header');
+});
+Route::get('/', function () {
+    return view('jumbotron');
+});
+Route::get('/', function () {
+    return view('main');
+});
+Route::get('/', function () {
+    return view('footer');
 });
