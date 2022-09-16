3fe2f265b11136cfb4a034344b0305598c793356
Eikcaj.1998 <jw220998@gmail.com>
2022-09-16 15:16:06 +0200

    fix alcune parti

--- routes/web.php
+++ routes/web.php
@@ -17,3 +17,13 @@ Route::get('/', function () {
     $title = 'home';
     return view('comics',compact('title'));
 });
+
+/* Route::get('/includes/{{id}}', function($id){
+    $products = config('pasta');
+    if (!is_numeric($id) || $id < 0 || $id >= count($products)){
+        abort(404);
+    }
+    $product = $products[$id];
+    return view('products.show',compact('product'));
+});
+ */
